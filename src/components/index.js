48f3adf09f7c757771ad1239cfa83023beb05ed9
Ray.Yan <ray.yan@RayYan-mbp.local>
2017-01-25 09:49:20 +0800

    react-router-rudux

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as App } from './App'
+export { default as Home } from './Home'
+export { default as Foo } from './Foo'
+export { default as Bar } from './Bar'
--- src/index.js
+++ src/index.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
-import Panel from './containers/Panel';
+import * as reducers from './reducers'
+import { App, Home, Foo, Bar } from './components'
 
-import configureStore from './store/configureStore';
+const reducer = combineReducers({
+  ...reducers,
+  routing: routerReducer
+})
+
+// const DevTools = createDevTools(
+//   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
+//     <LogMonitor theme="tomorrow" preserveScrollTop={false} />
+//   </DockMonitor>
+// )
+
+const store = createStore(
+  reducer
+)
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
-  <Provider store={configureStore}>
-      <Panel />
-  </Provider>
-  , document.getElementById('root'));
+  <Provider store={store}>
+    <div>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home}/>
+          <Route path="foo" component={Foo}/>
+          <Route path="bar" component={Bar}/>
+        </Route>
+      </Router>
+    </div>
+  </Provider>,
+  document.getElementById('root')
+)
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import { Provider } from 'react-redux';
+
+// import Panel from './containers/Panel';
+
+// import configureStore from './store/configureStore';
+
+// ReactDOM.render(
+//   <Provider store={configureStore}>
+//       <Panel />
+//   </Provider>
+//   , document.getElementById('root'));
