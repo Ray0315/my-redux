17755682b15a8e4f5027b82a679cc61e2a6aa19a
Ray.Yan <ray.yan@RayYan-mbp.local>
2017-02-09 10:03:07 +0800

    test 5

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,6 @@
 export { default as AppComponent } from '../app/app.component'
 export { default as Home } from './Home'
 export { default as Foo } from './pages/Foo'
-<<<<<<< HEAD
 export { default as Bar } from './pages/Bar'
-=======
-export { default as Bar } from './Bar'
->>>>>>> be37826a097d78d730e1bbf22213fdc2f8f5a72d
 export { default as Test } from './test'
 export { default as Ajax } from './Ajax'
